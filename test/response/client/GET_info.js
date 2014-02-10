7d9a3bd6e3d92781182afabd60b5fe1844912891
linkwisdom <linkwisdom@qq.com>
2014-02-10 11:13:37 +0800

    enable reponse timeout-a

---
+++ test/response/client/GET_info.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+    module.exports = function(path, param) {
+        return {
+            data: [123, 122, 135],
+            timeout: 2000
+        };
+    };
+});
